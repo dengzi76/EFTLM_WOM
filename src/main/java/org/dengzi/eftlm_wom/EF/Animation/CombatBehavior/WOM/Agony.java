d6d82856e792d889380ba1d90ea16bf9460654f4
S_SteelYard <1786447015@qq.com>
2026-06-07 02:19:25 +0800

    更新

--- src/main/java/org/dengzi/eftlm_wom/EF/Animation/CombatBehavior/WOM/Agony.java
+++ src/main/java/org/dengzi/eftlm_wom/EF/Animation/CombatBehavior/WOM/Agony.java
@@ -1,5 +1,6 @@
 package org.dengzi.eftlm_wom.EF.Animation.CombatBehavior.WOM;
 
+import net.EFTLM.EF.Animation.CombatBehavior.BehaviorsBuild;
 import org.dengzi.eftlm_wom.EF.Compat.CompatModList;
 import reascer.wom.gameasset.animations.weapons.AnimsAgony;
 import yesman.epicfight.world.capabilities.entitypatch.HumanoidMobPatch;
@@ -14,8 +15,9 @@ public class Agony {
 
     static {
         if (CompatModList.LoadedWOM()) {
-            Instance = CombatBehaviors.<HumanoidMobPatch<?>>builder().newBehaviorSeries(CombatBehaviors.BehaviorSeries.<HumanoidMobPatch<?>>builder().cooldown(20).weight(100.0F).canBeInterrupted(false).looping(false).nextBehavior(CombatBehaviors.Behavior.<HumanoidMobPatch<?>>builder().animationBehavior(AnimsAgony.AGONY_AUTO_1).withinDistance(0.0, 5.0)).nextBehavior(CombatBehaviors.Behavior.<HumanoidMobPatch<?>>builder().animationBehavior(AnimsAgony.AGONY_AUTO_2).withinDistance(0.0, 5.0)).nextBehavior(CombatBehaviors.Behavior.<HumanoidMobPatch<?>>builder().animationBehavior(AnimsAgony.AGONY_AUTO_3).withinDistance(0.0, 5.0)))
-                    .newBehaviorSeries(CombatBehaviors.BehaviorSeries.<HumanoidMobPatch<?>>builder().cooldown(20).weight(100.0F).canBeInterrupted(false).looping(false).nextBehavior(CombatBehaviors.Behavior.<HumanoidMobPatch<?>>builder().animationBehavior(AnimsAgony.AGONY_AIR_ATTACK_4).withinDistance(0.0, 5.0)).nextBehavior(CombatBehaviors.Behavior.<HumanoidMobPatch<?>>builder().animationBehavior(AnimsAgony.AGONY_AIR_ATTACK_1).withinDistance(0.0, 5.0)).nextBehavior(CombatBehaviors.Behavior.<HumanoidMobPatch<?>>builder().animationBehavior(AnimsAgony.AGONY_AIR_ATTACK_2).withinDistance(0.0, 5.0)));
+            Instance = CombatBehaviors.<HumanoidMobPatch<?>>builder().newBehaviorSeries(CombatBehaviors.BehaviorSeries.<HumanoidMobPatch<?>>builder().cooldown(20).weight(100.0F).canBeInterrupted(false).looping(false).nextBehavior(CombatBehaviors.Behavior.<HumanoidMobPatch<?>>builder().animationBehavior(AnimsAgony.AGONY_AUTO_1).withinDistance(0.0, 5.0)).nextBehavior(CombatBehaviors.Behavior.<HumanoidMobPatch<?>>builder().animationBehavior(AnimsAgony.AGONY_AUTO_2).withinDistance(0.0, 5.0)).nextBehavior(CombatBehaviors.Behavior.<HumanoidMobPatch<?>>builder().animationBehavior(AnimsAgony.AGONY_AUTO_3).withinDistance(0.0, 5.0)).nextBehavior(CombatBehaviors.Behavior.<HumanoidMobPatch<?>>builder().animationBehavior(AnimsAgony.AGONY_CLAWSTRIKE).withinDistance(0.0, 5.0)))
+                    .newBehaviorSeries(CombatBehaviors.BehaviorSeries.<HumanoidMobPatch<?>>builder().cooldown(20).weight(100.0F).canBeInterrupted(false).looping(false).nextBehavior(CombatBehaviors.Behavior.<HumanoidMobPatch<?>>builder().animationBehavior(AnimsAgony.AGONY_AIR_ATTACK_4).withinDistance(0.0, 5.0)).nextBehavior(CombatBehaviors.Behavior.<HumanoidMobPatch<?>>builder().animationBehavior(AnimsAgony.AGONY_AUTO_4).withinDistance(0.0, 5.0)).nextBehavior(CombatBehaviors.Behavior.<HumanoidMobPatch<?>>builder().animationBehavior(AnimsAgony.AGONY_RISING_EAGLE).withinDistance(0.0, 5.0)).nextBehavior(CombatBehaviors.Behavior.<HumanoidMobPatch<?>>builder().custom(BehaviorsBuild::canUseSkill).behavior((Patch) -> {Patch.playAnimationSynchronized(AnimsAgony.AGONY_SKY_DIVE, 0.0F);BehaviorsBuild.setCoolDown(Patch, 0);}).withinDistance(0.0, 24.0)).nextBehavior(CombatBehaviors.Behavior.<HumanoidMobPatch<?>>builder().animationBehavior(AnimsAgony.AGONY_AIR_ATTACK_2).withinDistance(0.0, 5.0)));
         }
     }
 }
+
