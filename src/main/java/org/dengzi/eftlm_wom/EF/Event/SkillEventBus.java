6650f82ce78621be8e89c1a1e5b87eb6d3928275
S_SteelYard <1786447015@qq.com>
2026-06-07 14:22:15 +0800

    更新

--- src/main/java/org/dengzi/eftlm_wom/EF/Event/SkillEventBus.java
+++ src/main/java/org/dengzi/eftlm_wom/EF/Event/SkillEventBus.java
@@ -1,12 +1,10 @@
 package org.dengzi.eftlm_wom.EF.Event;
 
 import net.EFTLM.EF.API.Event.MaidSkillBuildEvent;
-import net.EFTLM.EF.Skill.Passive.BladeClash;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
-import org.dengzi.eftlm_wom.EF.Skills.guard.Guard;
 import org.dengzi.eftlm_wom.EF.Skills.Passive.Meditation;
 
 @EventBusSubscriber(
@@ -20,6 +18,5 @@ public class SkillEventBus {
     @SubscribeEvent
     public static void MaidSkillBuild(MaidSkillBuildEvent event) {
         event.build(ResourceLocation.fromNamespaceAndPath("eftlm_wom", "meditation"), Meditation::new, Meditation.createBuilder());
-        event.build(ResourceLocation.fromNamespaceAndPath("eftlm_wom", "guard"), Guard::new, BladeClash.createBuilder());
     }
 }
--- src/main/java/org/dengzi/eftlm_wom/eftlm_wom.java
+++ src/main/java/org/dengzi/eftlm_wom/eftlm_wom.java
@@ -2,7 +2,6 @@ package org.dengzi.eftlm_wom;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.fml.common.Mod;
-import org.dengzi.eftlm_wom.EF.Skills.guard.MaidGuardRegistry;
 import org.slf4j.Logger;
 
 @Mod(eftlm_wom.MODID)
@@ -11,7 +10,6 @@ public class eftlm_wom {
     public static final String MODID = "eftlm_wom";
     private static final Logger LOGGER = LogUtils.getLogger();
     public eftlm_wom() {
-        MaidGuardRegistry.init();
         LOGGER.info("EFTLM_WOM mod loaded!");
     }
 }
